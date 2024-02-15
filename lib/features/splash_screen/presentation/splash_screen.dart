be4f6d66d35a8e0a2450b79ae26e58b8da243978
Aavhan Chatse <Aavhan.chatse@winjittechnologiespvtltd.local>
2024-02-15 14:22:49 +0530

    version library added

--- lib/features/splash_screen/presentation/splash_screen.dart
+++ lib/features/splash_screen/presentation/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ekyc/core/app_export.dart';
 import 'package:ekyc/core/dependency/injection.dart';
+import 'package:ekyc/core/helpers/package_info_helper.dart';
 import 'package:ekyc/core/storage/storage_key.dart';
 import 'package:ekyc/core/storage/storage_manager.dart';
 import 'package:ekyc/core/utils/extensions/context_extensions.dart';
@@ -11,25 +12,64 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:safe_device/safe_device.dart';
 
-class SplashScreen extends ConsumerWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    _navigateToLoginPage(context);
+  ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  String version = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getVersion();
+    _navigateToLoginPage();
+  }
+
+  Future<void> _getVersion() async {
+    version = await PackageInfoHelper.getVersion();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
-      body: Center(
-        child: Image.asset(
-          ImageConstants.pngSplashLogo,
-          width: 212.w,
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              ImageConstants.pngSplashLogo,
+              width: 212.w,
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Version: $version",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  void _navigateToLoginPage(BuildContext context) {
+  void _navigateToLoginPage() {
     Future.delayed(const Duration(seconds: 3), () {
       // callLaunchDetailsApi(context);
       context.go(AppRoutes.loginScreen);
@@ -82,3 +122,80 @@ class SplashScreen extends ConsumerWidget {
     return isJailBroken;
   }
 }
+
+// class SplashScreen extends ConsumerWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     _navigateToLoginPage(context);
+
+//     return Scaffold(
+//       backgroundColor: primaryColor,
+//       body: Stack(
+//         children: [
+//           Center(
+//             child: Image.asset(
+//               ImageConstants.pngSplashLogo,
+//               width: 212.w,
+//             ),
+//           ),
+//           const Text()
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _navigateToLoginPage(BuildContext context) {
+//     Future.delayed(const Duration(seconds: 3), () {
+//       // callLaunchDetailsApi(context);
+//       context.go(AppRoutes.loginScreen);
+//     });
+//   }
+
+//   void callLaunchDetailsApi(BuildContext context) async {
+//     final isJailBroken = await detectRootOrJailbreak();
+
+//     final body = LaunchDetailsRequest(
+//       rootedDevice: isJailBroken,
+//       deviceToken: "499dddb0-5ab1-4d04-90b6-87aadd4599ee",
+//     );
+
+//     final result = await getIt<LaunchDetails>().call(body);
+
+//     result.fold(
+//       (failure) {
+//         debugPrint("failure: ${failure.exception}");
+//         context.showErrorSnackBar(message: Strings.technicalError);
+//       },
+//       (LaunchDetailsResponse success) async {
+//         debugPrint("success in splash: $success");
+
+//         if (success.status?.isSuccess == true) {
+//           await getIt<AppStorageManager>().storeMap(
+//             key: StorageKey.LAUNCH_DETAILS,
+//             data: success.toJson(),
+//           );
+//           context.go(AppRoutes.loginScreen);
+//         } else {
+//           context.showErrorSnackBar(
+//             message: success.status?.message ?? Strings.globalErrorGenericMessageOne,
+//           );
+//         }
+
+//         // await getIt<SharedPreferences>().setBool(StorageConstant.isLoggedIn, true);
+//         // emit(LoginState.success(loginResponse: success));
+//       },
+//     );
+//   }
+
+//   Future<bool> detectRootOrJailbreak() async {
+//     bool isJailBroken = await SafeDevice.isJailBroken;
+//     debugPrint("jailBroken: $isJailBroken");
+
+//     bool isRealDevice = await SafeDevice.isRealDevice;
+//     debugPrint("realDevice: $isRealDevice");
+
+//     return isJailBroken;
+//   }
+// }
