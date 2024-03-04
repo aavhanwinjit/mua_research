33bcc45189d935582a0319317a88353d78e28d5f
Aavhan Chatse <Aavhan.chatse@winjittechnologiespvtltd.local>
2024-03-04 16:48:31 +0530

    contact us action sheet added

--- lib/core/helpers/signature_source_actionsheet_helper.dart
+++ lib/core/helpers/signature_source_actionsheet_helper.dart
@@ -1,4 +1,5 @@
 import 'package:ekyc/core/app_export.dart';
+import 'package:ekyc/core/helpers/url_launcher_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
@@ -83,6 +84,52 @@ class ActionSheetHelper {
             onPressed: onLibraryPressed,
             child: const Text(
               Strings.uploadFromLibrary,
+              style: TextStyle(
+                color: iosButtonBlueTextColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static void showContactUsActionSheet(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+        cancelButton: CupertinoActionSheetAction(
+          onPressed: () {
+            context.pop();
+          },
+          isDefaultAction: true,
+          child: const Text(
+            Strings.cancel,
+            style: TextStyle(
+              color: iosButtonBlueTextColor,
+            ),
+          ),
+        ),
+        actions: <CupertinoActionSheetAction>[
+          CupertinoActionSheetAction(
+            onPressed: () {
+              context.pop();
+              UrlLauncherHelper.launchPhone();
+            },
+            child: const Text(
+              Strings.helpContact,
+              style: TextStyle(
+                color: iosButtonBlueTextColor,
+              ),
+            ),
+          ),
+          CupertinoActionSheetAction(
+            onPressed: () {
+              context.pop();
+              UrlLauncherHelper.launchMail();
+            },
+            child: const Text(
+              Strings.helpEmail,
               style: TextStyle(
                 color: iosButtonBlueTextColor,
               ),
