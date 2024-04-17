04d75d4a385969055611f66d71abda4f22a625d1
dipak.kulkarni <dipak.kulkarni@winjit.com>
2024-04-17 14:38:20 +0530

    firebase crashlytics added in iOS

--- lib/core/dependency/injection.dart
+++ lib/core/dependency/injection.dart
@@ -18,8 +18,8 @@ abstract class MAUEngineModule {
   AppConfig getAppConfig() {
     return const AppConfig(
       appName: "MAU",
-      baseUrl: "http://192.168.10.222:9890",// !DIPAK SYSTEM
-      // baseUrl: "http://192.168.0.134:9890",
+      // baseUrl: "http://192.168.10.222:9890",// !DIPAK SYSTEM
+      baseUrl: "http://192.168.0.134:9890",
       // baseUrl: "http://10.100.1.121/agentapi/",
     );
   }
