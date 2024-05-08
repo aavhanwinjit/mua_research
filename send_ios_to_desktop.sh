72cf7798ffab3240ed11db3390e0a9b35a7bf1be
dipak.kulkarni <dipak.kulkarni@winjit.com>
2024-05-08 17:28:40 +0530

    shell script file and export option file changes

--- send_ios_to_desktop.sh
+++ send_ios_to_desktop.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-flutter clean && flutter build ipa --export-options-plist=ExportOptions.plist
+fvm flutter clean && fvm flutter build ipa --export-options-plist=ExportOptions.plist
+
 if [[ $? == 0 ]]; then
     echo "Copying IPA...."
-    cp -f $PWD/build/ios/ipa/ekyc.ipa $HOME/Desktop/
+    # Remove spaces from filename
+    src="$PWD/build/ios/ipa/MUA KYC.ipa"
+    dest="$HOME/Desktop/"
+    filename=$(basename "$src")
+    filename=${filename// /_}
+    cp -f "$src" "$dest$filename"
     echo "IPA copied."
+    
     echo "Renaming IPA...."
-    mv $HOME/Desktop/ekyc.ipa $HOME/Desktop/ekyc-$(date +%F).ipa
+    current_date=$(date +%d%m%Y)
+    new_filename=${filename// /}
+    new_filename=${new_filename//ipa/ipa}
+    mv "$dest${filename// /}" "$dest/MUA_KYC_${current_date}.ipa"
 else
     echo "Flutter IPA Build Failed"
 fi
