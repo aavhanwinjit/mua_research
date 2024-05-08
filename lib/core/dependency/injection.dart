d3b2aabe36e9734deb9a9602f503e147cd6d6b42
Nikhil Sinha <Nikhil.Sinha@winjittechnologiespvtltd.local>
2024-05-08 13:32:59 +0530

    forgot MPIN issue fixed

--- lib/core/dependency/injection.dart
+++ lib/core/dependency/injection.dart
@@ -19,9 +19,9 @@ abstract class MAUEngineModule {
     return const AppConfig(
       appName: "MAU",
       // baseUrl: "http://192.168.10.222:9890",// !DIPAK SYSTEM
-      // baseUrl: "http://192.168.0.134:9890", // DEV
+      baseUrl: "http://192.168.0.134:9890", // DEV
       // baseUrl: "http://10.100.1.121/agentapi/",
-      baseUrl: "http://10.15.0.124/agentapi/", // UAT
+      // baseUrl: "http://10.15.0.124/agentapi/", // UAT
     );
   }
 
